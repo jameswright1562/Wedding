8df7485793576da70de7e3849f8dfdc74b3c6bd5
Your Name <you@example.com>
2026-02-10 14:57:37 +0000

    Commit

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "https://jfqqkujeplewiazefbkd.supabase.co",
-  key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmcXFrdWplcGxld2lhemVmYmtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0NzQwNzMsImV4cCI6MjA4NjA1MDA3M30.z3O3B07fRzJSxeSJseFKKpP74BT9Ob_qXRZ3i27MAQE",
+  url: "",
+  key: "",
 };
